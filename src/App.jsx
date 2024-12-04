850ea1ac80844075ffb07a58c97d57b6cfe73808
Bhawanishankar <swastikin1@gmail.com>
2024-12-04 16:35:35 +0530

    web created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+// import Mainsection from './Mainsection'
+const App = () => {
+  return (
+    <div>
+      
+      <Home/>
+      {/* <Mainsection/> */}
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/main.jsx
@@ -0,0 +1,67 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import { Outlet, RouterProvider, createBrowserRouter  } from 'react-router-dom'
+import About from './Components/About/About.jsx'
+import Services from './Components/Sevices/Services'
+import Catalog from './Components/Catalog/Catalog.jsx'
+import Achievement from './Components/Achievement/Achievement.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+import Career from './Components/career/Career.jsx'
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet/>
+    </div>
+    
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/about',
+        element:<About/>
+      },
+      {
+        path: '/service',
+        element:<Services/>
+      },
+      {
+        path: '/catalog',
+        element:<Catalog/>
+      },
+      {
+        path: '/achievement',
+        element:<Achievement/>
+      },
+      {
+        path: '/contact',
+        element:<Contact/>
+      },
+      {
+        path:'/career',
+        element:<Career/>
+      }
+    ]
+  }
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
